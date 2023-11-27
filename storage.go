a28888146423274c2e9164f9142af260451efd54
Laura <milanbartky@gmail.com>
2023-11-27 21:26:20 +0100

    support symlinks

---
+++ storage.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+)
+
+const StorageDirectory = "./storage"
+
+func storageAbs() (string, error) {
+	info, err := os.Lstat(StorageDirectory)
+	if err != nil {
+		return "", err
+	}
+
+	if info.Mode()&os.ModeSymlink != 0 {
+		return os.Readlink(StorageDirectory)
+	}
+
+	return filepath.Abs(StorageDirectory)
+}
